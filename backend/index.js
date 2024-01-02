5d96a29c3dabeb456d2497106968c5a34ccfb1b5
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2024-01-02 17:58:52 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const routes = require("./routes/routes");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.listen(3000, () => {
   console.log(`Server Started at ${3000}`);
 });
